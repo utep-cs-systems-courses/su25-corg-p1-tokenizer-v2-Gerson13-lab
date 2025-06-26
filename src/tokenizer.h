d7868d00c7e8974f75253e4b11b8ee0c4703b472
Gerson13-lab <gersoneli1378@gmail.com>
2025-06-26 14:26:44 -0600

    token terminated implemented

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -23,18 +23,27 @@ int non_space_char(char c)
    space-separated token in zero-terminated str.  Return a zero pointer if 
    str does not contain any tokens. */
 char *token_start(char *str){
-  char *p = str;
+  char *p = str; 
   while(space_char(*p){
       p++;
     }
-    if (*p == '\0'){
+  if (*p == '\0'){
       return NULL;
     }
-    return p;
+  return p;
 }
 
 /* Returns a pointer terminator char following *token */
-char *token_terminator(char *token);
+  char *token_terminator(char *token){
+    /*checks if empty*/
+    if (!token){ return NULL;}
+    
+    char *p = token;
+    while (*p != '\0' && non_space_char(*p){
+	p++;
+      }
+      return p;
+  }
 
 /* Counts the number of tokens in the string argument. */
 int count_tokens(char *str);
