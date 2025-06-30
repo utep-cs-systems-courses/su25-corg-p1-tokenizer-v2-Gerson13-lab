26150896a5e82e8b6bce816ccca0903532652305
Gerson13-lab <gersoneli1378@gmail.com>
2025-06-30 01:06:09 -0600

    add_history implemented

--- src/history.h
+++ src/history.h
@@ -16,14 +16,31 @@ List* init_history(){
   List* history = (List*)malloc(sizeof(List));
   history -> root = NULL; /*initlizes root to null*/
   return history;
-
-
 }
+
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  static int counter = 0 /* holds current counter */;
+  Item *new_item = (Item*)malloc(sizeof(Item));
+  new_item -> id = counter++;
+  new_item -> str = strdup(str); /*allocates memory for string */
+  new_item -> next = NULL;
+
+  if (list -> root = NULL){
+    list -> root = new_item;
+  }
+  else {
+    Item *current= list -> root;
+    while (current -> next != NULL){
+      current = current -> next;
+    }
+    current -> next = new_item;
+  }
+  
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
