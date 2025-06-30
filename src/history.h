504a5cd95da0260913999e0d51b0b53578d727bd
Gerson Cruz <72302679+Gerson13-lab@users.noreply.github.com>
2025-06-30 01:48:28 -0600

    get_history implemented

--- src/history.h
+++ src/history.h
@@ -17,18 +17,42 @@ List* init_history(){
   history -> root = NULL; /*initlizes root to null*/
   return history;
 
-
 }
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  static int counter = 0;
+  Item *new_item -> id = counter++;
+  new_item -> str = strdup(str); /*allocates memory for str*/
+  new_item -> next = NULL;
+
+  if(list -> root = NULL){
+    list -> root = new_item;
+  }
+  else {
+    Item *current = list -> root;
+    while(current) {
+      current = current -> next;
+    }
+    current -> next = new_item;
+  }
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id);
+char *get_history(List *list, int id){
+  Item *current = list -> root; /*gets first item of list*/
+  while(current){
+    if(current -> id == id){
+      return current -> str;
+    }
+    current = current -> next /*goes to next entry if id dont match*/
+  }
+  return NULL; /*id not found in list*/;
+}
 
 /*Print the entire contents of the list. */
 void print_history(List *list);
